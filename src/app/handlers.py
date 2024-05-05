9173371af5c0bcb8ebd902121c5cd696f8a1f94b
Dmitrii <lagodnov@gmail.com>
2024-05-06 00:30:27 +0300

    final

--- src/app/handlers.py
+++ src/app/handlers.py
@@ -23,16 +23,15 @@ class DelTask(StatesGroup):
 async def bot_start(message: Message):
     await rq.set_user(message.from_user.id)
     await message.reply(
-        'Привет!\nСоздадим первую задачу?\nНажимай на /add',
-        reply_markup=kb
+        "Привет!\nСоздадим первую задачу?\nНажимай на /add", reply_markup=kb
     )
 
 
 # Обработчик команды /add для добавления задачи
-@router.message(Command('add'))
+@router.message(Command("add"))
 async def create_task(message: Message, state: FSMContext):
     await state.set_state(CreateTask.task)
-    await message.answer('Введите текст задачи:')
+    await message.answer("Введите текст задачи:", reply_markup=kb)
 
 
 @router.message(CreateTask.task)
@@ -40,33 +39,35 @@ async def add_task(message: Message, state: FSMContext):
     tg_id: int = message.from_user.id
     task: str = message.text
     await rq.set_task(tg_id=tg_id, task=task)
-    await message.answer(f'Задача:\n{task}\nCохранена.')
+    await message.answer(f"Задача:\n{task}\nCохранена.", reply_markup=kb)
     await state.clear()
 
 
 # Обработчик команды /tsk для добавления задачи
-@router.message(Command('tsk'))
+@router.message(Command("tsk"))
 async def get_tasks(message: Message, state: FSMContext):
     tg_id: int = message.from_user.id
     tasks_generator = rq.get_task(tg_id=tg_id)
 
     async for tasks in tasks_generator:
         if not tasks:
-            await message.answer('Текущих задач нет.')
+            await message.answer("Текущих задач нет.", reply_markup=kb)
             break
         for obj_task in tasks:
             number: int = obj_task.id
             task: str = obj_task.task
-            await message.answer(f'Задача: № {number}\n{task}.')
+            await message.answer(f"Задача: № {number}\n{task}.",
+                                 reply_markup=kb)
 
     await state.clear()
 
 
 # Обработчик команды /del для добавления задачи
-@router.message(Command('del'))
+@router.message(Command("del"))
 async def delete_task(message: Message, state: FSMContext):
     await state.set_state(DelTask.task)
-    await message.answer('Введите задачи для удаления через пробел:')
+    await message.answer("Введите задачи для удаления через пробел:",
+                         reply_markup=kb)
 
 
 @router.message(DelTask.task)
@@ -74,5 +75,5 @@ async def del_tasks(message: Message, state: FSMContext):
     tg_id: int = message.from_user.id
     tasks: list = list(map(int, message.text.split()))
     await rq.del_task(tg_id=tg_id, id_tasks=tasks)
-    await message.answer('Задачи удалены.')
+    await message.answer("Задачи удалены.", reply_markup=kb)
     await state.clear()
